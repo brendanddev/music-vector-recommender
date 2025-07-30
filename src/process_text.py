79bef27b8392978ed8ec7ed270a38737dc31c923
Brendan Dileo <dileob23@gmail.com>
2025-07-30 19:48:11 -0400

    Implement text cleaning function

--- src/process_text.py
+++ src/process_text.py
@@ -4,3 +4,17 @@ process_text.py
 
 Brendan Dileo, July 2025
 """
+
+import os
+import re
+import pickle 
+
+
+def clean_text(text) -> str:
+    """  Cleans input text by converting to lowercase and removing html tags """
+    cleaned_text = text.lower()
+    cleaned_text = re.sub(r'<[^>]+>', '', cleaned_text)
+    return cleaned_text
+
+
+def vectorize_songs(songs, save_path="vectors.pkl"):
