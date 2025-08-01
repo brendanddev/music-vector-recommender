b8a394cdb997308abc4ed1dc654daa27c7e921b6
Brendan Dileo <dileob23@gmail.com>
2025-08-01 11:02:07 -0400

    Add class declr and class methods

--- src/config/config.py
+++ src/config/config.py
@@ -10,3 +10,17 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+class Config:
+    SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+    SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+    
+    @classmethod
+    def validate(cls):
+        if not Config.SPOTIFY_CLIENT_ID or not Config.SPOTIFY_CLIENT_SECRET:
+            raise ValueError("Missing SPOTIFY_CLIENT_ID or SPOTIFY_CLIENT_SECRET environment variables")
+     
+    @classmethod
+    def _debug_print(cls):
+        print("SPOTIFY_CLIENT_ID:", cls.SPOTIFY_CLIENT_ID)
+        print("SPOTIFY_CLIENT_SECRET:", cls.SPOTIFY_CLIENT_SECRET)
