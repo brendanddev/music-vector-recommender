8e702ce890787f5f17458bd1c88e0df90735157e
Brendan Dileo <dileob23@gmail.com>
2025-08-01 11:01:55 -0400

    Setup config

---
+++ src/config/config.py
@@ -0,0 +1,12 @@
+
+""" 
+config.py
+Loads and validates configuration settings for the music vector recommender system
+
+Brendan Dileo, August 2025
+"""
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
