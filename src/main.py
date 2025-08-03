2fa834869c85ccc69e6d4c07e99710d7975839cb
Brendan Dileo <dileob23@gmail.com>
2025-08-03 17:41:14 -0400

    Add main function

--- src/main.py
+++ src/main.py
@@ -9,6 +9,11 @@ Brendan Dileo, August 2025
 from utils.vectorizer import vectorize_songs
 from utils.load_data import load_songs
 from config.config import Config
+from utils.recommender import (
+    init_recommendations,
+    display_recommendations,
+    generate_recommendations
+)
 
 def get_user_choice(recs):
     """ Prompts the user to select a song from the list of recommendations """
@@ -30,3 +35,28 @@ def display_song(idx, songs):
     print("-" * 40)
     print(song.get("lyrics", "[No lyrics available]"))
     print("-" * 40)
+    
+
+def main():
+    """ Main function to run the music vector recommender system """
+    Config.validate()
+    songs = load_songs(Config.DATA_PATH, num=Config.NUM_SONGS, filetype='csv')
+    print(f"\nLoaded {len(songs)} songs.")
+
+    print("Vectorizing lyrics...")
+    tfidf_matrix, _ = vectorize_songs(songs, save_path=Config.VECTORS_PATH)
+    print("Vectorization complete!\n")
+
+    recs = init_recommendations(Config.NUM_RECS, songs)
+
+    while True:
+        display_recommendations(recs, songs)
+        choice = get_user_choice(recs)
+
+        display_song(recs[choice], songs)
+        input("Press Enter to get new recommendations...")
+
+        recs = generate_recommendations(recs[choice], tfidf_matrix, songs, Config.NUM_RECS)
+
+if __name__ == "__main__":
+    main()
